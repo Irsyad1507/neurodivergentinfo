13d58f1c6a78a2b998ffe919f3fbe0c580c41ab6
Amirul Irsyad <amirulirsyad157@gmail.com>
2025-08-30 16:06:17 +0800

    Added bio field for (custom) user profile model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
